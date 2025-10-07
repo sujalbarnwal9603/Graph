01e72796ac95e3d7bf16a143aa70d2fe5a1ed22a
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-10-07 21:21:01 +0530

    Undirected Unweighted & Weighted Graph

---
+++ Graph/01_adjaencyMatrix.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Adjacency Matrix
+
+
+// Undirected Unweighted Graph
+
+int main(){
+    int vertex, edges;
+    cin >> vertex>>edges;
+    
+    vector<vector<bool> >AdjMat(vertex, vector<bool>(vertex,0));
+    
+    int u,v;
+    for(int i=0;i<edges;i++){
+        cin >> u>> v;
+        AdjMat[u][v]=1;
+        AdjMat[v][u]=1;
+    }
+    
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout << AdjMat[i][j] <<" ";
+        }
+        cout << endl;
+    }
+}
+
+// Undirected Weighted Graph
+
+int main(){
+    int vertex, edges;
+    cin >> vertex>>edges;
+    
+    vector<vector<int> >AdjMat(vertex, vector<int>(vertex,0));
+    
+    int u,v, weight;
+    for(int i=0;i<edges;i++){
+        cin >> u>> v>> weight;
+        AdjMat[u][v]=weight;
+        AdjMat[v][u]=weight;
+    }
+    
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout << AdjMat[i][j] <<" ";
+        }
+        cout << endl;
+    }
+}
