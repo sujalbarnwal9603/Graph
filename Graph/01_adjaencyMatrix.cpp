7a399b39a3c6b1a325a9bd252700db74c17ecb6e
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-10-07 21:33:01 +0530

    Direted graph

--- Graph/01_adjaencyMatrix.cpp
+++ Graph/01_adjaencyMatrix.cpp
@@ -52,3 +52,49 @@ int main(){
         cout << endl;
     }
 }
+
+
+// Directed Unweighted Graph
+
+int main(){
+    int vertex, edges;
+    cin >> vertex>>edges;
+    
+    vector<vector<bool> >AdjMat(vertex, vector<bool>(vertex,0));
+    
+    int u,v;
+    for(int i=0;i<edges;i++){
+        cin >> u>> v;
+        AdjMat[u][v]=1;
+    }
+    
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout << AdjMat[i][j] <<" ";
+        }
+        cout << endl;
+    }
+}
+
+// Directed Weighted Graph
+
+int main(){
+    int vertex, edges;
+    cin >> vertex>>edges;
+    
+    vector<vector<int> >AdjMat(vertex, vector<int>(vertex,0));
+    
+    int u,v, weight;
+    for(int i=0;i<edges;i++){
+        cin >> u>> v>> weight;
+        AdjMat[u][v]=weight;
+    }
+    
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout << AdjMat[i][j] <<" ";
+        }
+        cout << endl;
+    }
+}
+
