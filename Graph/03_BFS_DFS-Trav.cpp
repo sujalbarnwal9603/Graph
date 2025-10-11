73881a29ed64b3145c2079341445202b5688f4a5
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-10-11 14:13:22 +0530

    final dfs

--- Graph/03_BFS_DFS-Trav.cpp
+++ Graph/03_BFS_DFS-Trav.cpp
@@ -39,10 +39,17 @@ void DFS(int node, vector<int> adj[],vector<bool>&visited, vector<int>&ans){
   visited[node]=1;
   ans.push_back(node);
   for(int j=0;j<adj[node].size();j++){
-    if(!visited[adj[node[j]]]){
+    if(!visited[adj[node][j]]){
       DFS(adj[node][j],adj,visited,ans);
     }
   }
+}
+
+vector<int> DFSGraph(int v, vector<int> adj[]){
+  vector<bool> visited(v,0);
+  vector<int> ans;
+  DFS(0,adj,visited,ans);
+  return ans;
 }
 
 int main(){
